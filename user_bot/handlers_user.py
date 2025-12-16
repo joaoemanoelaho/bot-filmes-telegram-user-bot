898d40b14004dc65cba1dfe4ff6f4a0f17e8386e
joaoemanoelaho <joaoemanoelaho@gmail.com>
2025-12-16 13:06:14 -0300

    Adiciona botão de compartilhamento na estrutura do teclado de navegação

--- user_bot/handlers_user.py
+++ user_bot/handlers_user.py
@@ -286,10 +286,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                         if nav_row:
                             keyboard.append(nav_row)
 
-                        keyboard = [
+                        keyboard.append([
                             [ InlineKeyboardButton("🍿 Relacionados", callback_data=f"related_{series_id_for_related}_series"), InlineKeyboardButton("Compartilhar ❤️", switch_inline_query=f"ep_card:{episode_id}") ],
                             [ InlineKeyboardButton(fav_btn_text, callback_data=f"fav_toggle_{fav_unique_code}") ]
-                        ]
+                        ])
                         
                         video_reply_markup = InlineKeyboardMarkup(keyboard)
 
